df84b1f5b2749787ff6957e39cee44ccee5119ea
changeon.kim <lygerastia5@gmail.com>
2022-03-19 21:27:15 +0900

    comment: 무한스크롤 구현 방법

--- client/src/hooks/useIntersection.ts
+++ client/src/hooks/useIntersection.ts
@@ -1,6 +1,14 @@
 import { RefObject, useCallback, useEffect, useRef, useState } from 'react'
 
 const useIntersection = (targetRef: RefObject<HTMLElement>) => {
+  /*
+    무한스크롤
+    1
+     - scrollTop + window.clientHeight 등을 이용해 끝에 도달했는지 계산
+     - eventHandler (scroll) => 감시. throttle / debounce -> 쓰레드 메모리 차지, 성능 저하
+    2 interSectionObserver
+     - 이벤트 등록 X 브라우저에서 제공하는 별개의 감시자. 성능 ⬆
+*/
   const observerRef = useRef<IntersectionObserver>()
   const [intersecting, setIntersecting] = useState(false)
 
